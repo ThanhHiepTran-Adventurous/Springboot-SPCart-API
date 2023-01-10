ba37e11293777e1be85f1ad19cfa93f812e716da
Thanh-hiep-NT-0105 <nguyenhiep0456@gmail.com>
2023-01-10 23:55:34 +0700

    add entity and package

---
+++ demo.springboot/src/main/java/com/springboot/shoppingcart/api/controller/ShoppingCartController.java
@@ -0,0 +1,12 @@
+package com.springboot.shoppingcart.api.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ShoppingCartController {
+
+
+}
---
+++ demo.springboot/src/main/java/com/springboot/shoppingcart/api/entity/Customer.java
@@ -0,0 +1,26 @@
+package com.springboot.shoppingcart.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+}
